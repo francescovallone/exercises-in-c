6c92afe8d07bd62ce6709455b52f4ee8c4392199
Francesco Vallone <vallonefrancesco587@gmail.com>
2018-10-30 09:29:30 +0100

    Sistemata l'indentazione

--- 29-10-2018/esercizio9.c
+++ 29-10-2018/esercizio9.c
@@ -8,9 +8,9 @@ int main(){
         for (l = 1; l < m; l++) {
         	// Le formule le ho prese da wikipedia e.e
         	a = (m*m) - (l*l);
-			b = 2 * m * l;
-			c =  (m*m) + (l*l);
-			// Verifica se il valore della c supera il valore assegnato a n dall'utente, se si verifica la condizione salta quel valore.
+		b = 2 * m * l;
+		c =  (m*m) + (l*l);
+		// Verifica se il valore della c supera il valore assegnato a n dall'utente, se si verifica la condizione salta quel valore.
             if (c > n){
             	break;
             }
