c33a44abf6c057ba58a2fe94b51cf63052cddea8
francescovallone <vallonefrancesco587@gmail.com>
2018-11-02 20:24:26 +0100

    Esercizio Assegnato in data 02-11-2018

---
+++ 02-11-2018/esercizio1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int primo(int a);
+
+int main(){
+	int a, i;
+	scanf("%d", &a);
+	for(i = 2; i<=a;i++){
+		if(primo(i)){
+			printf("%d ", i);
+		}
+	}
+	return 0;
+}
+
+int primo(int a){
+	int i, j=0;
+	for(i = 2; i<=a/2; i++){
+		if(a%i == 0){
+			j = 1;
+			break;	
+		}
+	}
+	if(j == 0){
+		return i;
+	}
+}
