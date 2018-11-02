2e99448a5222892b3e23d1a871b754f43a44742c
francescovallone <vallonefrancesco587@gmail.com>
2018-11-02 10:27:29 +0100

    Piccolo Fix

--- 31-10-2018/esercizio5.c
+++ 31-10-2018/esercizio5.c
@@ -4,7 +4,7 @@ int main(){
 	int k, j, i;
 	scanf("%d", &k);
 	// Stampo tutti i valori seguiti da ,
-	for (j=1, i=1; j<=(k*k); j++, i+=1){
+	for (j=1, i=1; j<=(k*k); j++, i++){
 		if(i == k){
 			printf("%d \n", j);
 			i = 0;
