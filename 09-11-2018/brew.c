9fd5db014e22c9a549970b21566facbf2eb26843
francescovallone <vallonefrancesco587@gmail.com>
2018-11-09 16:39:35 +0100

    Libraria Creata con tutte le funzioni richieste.

---
+++ 09-11-2018/brew.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+
+int mcd(int a, int b){
+	int m;
+	if(a > b){
+		a = a ^ b;
+		b = a ^ b;
+		a = a ^ b;
+	}
+	for(m=a; a%m || b%m; m--);
+	return m;
+}
+
+
+int mcm(int a, int b){
+	int m, t=1;
+	if(a > b){
+		a = a ^ b;
+		b = a ^ b;
+		a = a ^ b;
+	}
+	for(m=b; (m%a)||(m%b); m++);
+	return m;
+}
+
+
+void analyze(void){
+	int i, r, c=0, d=0, temp, pos=0, neg=0, even=0, odd=0, none=0;
+
+	while(scanf("%d", &r)){
+		
+		if(r < 0){
+			neg++;
+		}else if(r == 0){
+			none++;
+		}else{
+			pos++;
+		}
+		if(r != 0 && !(r%2)){
+			even++;
+		}else if(r != 0){
+			odd++;
+		}
+		if(r < temp){
+			d++;
+		}else if(r > temp){
+			c++;
+		}
+		temp = r;
+	}
+	if(d > 0 && c == 0){
+		printf("La funzione e' decrescente \n");
+	}else if(c > 0 && d == 0){
+		printf("La funzione e' crescente \n");
+	}else{
+		printf("La funzione non e' ne crescente ne decrescente \n");
+	}
+	printf("I numeri negativi sono %d \n", neg);
+	printf("I numeri positivi sono %d \n", pos);
+	printf("I numeri nulli sono %d \n", none);
+	printf("I numeri pari sono %d \n", even);
+	printf("I numeri dispari sono %d", odd);
+}
+
+
+void fibonacci(int a){
+	int i, t=0, s=1, r=0;
+	for(i=1; i<=a; i++){
+		printf("%d \t", t);
+		r = t + s;
+		t = s;
+		s = r;
+	}
+}
---
+++ 09-11-2018/brew.h
@@ -0,0 +1,8 @@
+// Massimo Comun Divisore
+int mcd(int a, int b);
+// Minimo Comune Multiplo
+int mcm(int a, int b);
+// Analisi di una successione
+void analyze();
+// Valori dei primi n numeri della successione di fibonacci
+void fibonacci(int a);
