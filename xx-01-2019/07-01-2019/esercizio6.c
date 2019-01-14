f1acf572283277955cdd68f18dbd1c35dfe0edb1
francescovallone <vallonefrancesco587@gmail.com>
2019-01-14 16:13:29 +0100

    Fix ora verifica tutti i numeri :green_heart:

--- xx-01-2019/07-01-2019/esercizio6.c
+++ xx-01-2019/07-01-2019/esercizio6.c
@@ -5,13 +5,19 @@ int controlla_matrice(int *A, int n){
     /* Ammetto che siano tutti diversi ponendo la flag ad 1 */
     int flag = 1;
     /* La flag verrà usata come variabile del return e come controllo per il for */
-    for(int i = 0; i<n-1 && flag; i++){
+    for(int i = 0, k=0; i<n && k<n && flag; i++){
         /* Se il valore di A in posizione i e di A in posizione i+1
             sono diversi allora continua il ciclo altrimenti esce dal ciclo*/
-        if(A[i] != A[i+1]){
-            flag = 1;
-        }else{
-            flag = 0;
+        if(k != i){
+            if(A[k] != A[i]){
+                flag = 1;
+            }else{
+                flag = 0;
+            }
+        }
+        if(i<n){
+            i = 0;
+            k++;
         }
     }
     /* Alla fine la flag verrà ritornata sia che sia 1 sia che sia 0 */
